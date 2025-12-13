f486a3d4ebacea940a5985f1e6f36e56bdaf111c
Christopher Pope <christopher@thepopes.org>
2025-12-13 11:02:46 -0500

    Add ReadAll() for companies service

--- GoodProductsApi.BusinessLogic/Services/CompaniesService.cs
+++ GoodProductsApi.BusinessLogic/Services/CompaniesService.cs
@@ -1,16 +1,40 @@
-﻿using GoodProductsApi.DataAccess.Persistence.Interfaces;
+﻿using FluentResults;
+using GoodProductsApi.BusinessLogic.DTOs;
+using GoodProductsApi.BusinessLogic.Mappers.Interfaces;
+using GoodProductsApi.BusinessLogic.Services.Interfaces;
+using GoodProductsApi.DataAccess.Persistence.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace GoodProductsApi.BusinessLogic.Services;
 
-internal sealed class CompaniesService
+internal sealed class CompaniesService : ICompaniesService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly ILogger<CompaniesService> _logger;
+    private readonly ICompanyMapper _companyMapper;
 
-    public CompaniesService(IUnitOfWork unitOfWork, ILogger<CompaniesService> logger)
+    public CompaniesService(IUnitOfWork unitOfWork,
+        ILogger<CompaniesService> logger,
+        ICompanyMapper companyMapper)
     {
         _unitOfWork = unitOfWork;
         _logger = logger;
+        _companyMapper = companyMapper;
+    }
+
+    public async Task<Result<List<CompanyDto>>> ReadAll(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var companyEntities = await _unitOfWork.Companies.ReadAll(cancellationToken);
+
+            return _companyMapper.FromEntities(companyEntities);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unable to read Companies");
+
+            return Result.Fail("Unable to read Companies");
+        }
     }
 }
---
+++ GoodProductsApi.BusinessLogic/Services/Interfaces/ICompaniesService.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using GoodProductsApi.BusinessLogic.DTOs;
+
+namespace GoodProductsApi.BusinessLogic.Services.Interfaces;
+
+public interface ICompaniesService
+{
+    Task<Result<List<CompanyDto>>> ReadAll(CancellationToken cancellationToken);
+}
