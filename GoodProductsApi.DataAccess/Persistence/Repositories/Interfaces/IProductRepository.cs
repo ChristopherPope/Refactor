1c56c2e832f17655f4d31224352d7be2b56be546
Christopher Pope <christopher@thepopes.org>
2025-12-13 09:45:20 -0500

    Add basic ProductRepository

---
+++ GoodProductsApi.DataAccess/Persistence/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoodProductsApi.DataAccess.Entities;
+
+namespace GoodProductsApi.DataAccess.Persistence.Repositories.Interfaces;
+
+public interface IProductRepository : IRepository<Product>
+{
+    Task<List<Product>> ReadAll(CancellationToken cancellationToken);
+    Task<Product?> ReadById(int productId, CancellationToken cancellationToken);
+}
---
+++ GoodProductsApi.DataAccess/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using GoodProductsApi.DataAccess.Entities;
+using GoodProductsApi.DataAccess.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodProductsApi.DataAccess.Persistence.Repositories;
+
+internal sealed class ProductRepository : Repository<Product>, IProductRepository
+{
+    public ProductRepository(ProductsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<List<Product>> ReadAll(CancellationToken cancellationToken)
+    {
+        return await Entities
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Product?> ReadById(int productId, CancellationToken cancellationToken)
+    {
+        return await Entities
+            .Where(p => p.Id == productId)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+}
