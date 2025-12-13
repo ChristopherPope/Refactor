9fda1ce8c05e45746f3ed4d22d35bb2d50a220d2
Christopher Pope <christopher@thepopes.org>
2025-12-13 10:43:57 -0500

    Add skeleton Companies and Products business logic services

---
+++ GoodProductsApi.BusinessLogic/Services/CompaniesService.cs
@@ -0,0 +1,16 @@
+﻿using GoodProductsApi.DataAccess.Persistence.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace GoodProductsApi.BusinessLogic.Services;
+
+internal sealed class CompaniesService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<CompaniesService> _logger;
+
+    public CompaniesService(IUnitOfWork unitOfWork, ILogger<CompaniesService> logger)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+}
---
+++ GoodProductsApi.BusinessLogic/Services/ProductsService.cs
@@ -0,0 +1,16 @@
+﻿using GoodProductsApi.DataAccess.Persistence.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace GoodProductsApi.BusinessLogic.Services;
+
+internal sealed class ProductsService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<ProductsService> _logger;
+
+    public ProductsService(IUnitOfWork unitOfWork, ILogger<ProductsService> logger)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+}
