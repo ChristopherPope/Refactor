a6dccb640b6ad94bf25288f586cc3588802def30
Christopher Pope <christopher@thepopes.org>
2025-12-13 15:30:27 -0500

    Add Product API

---
+++ GoodProductsApi/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using GoodProductsApi.BusinessLogic.DTOs;
+using GoodProductsApi.BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodProductsApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ProductsApiController
+{
+    private readonly IProductsService _productsService;
+
+    public ProductController(IProductsService productsService)
+    {
+        _productsService = productsService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductDto>>> Get(CancellationToken cancellationToken = default)
+    {
+        var r = await _productsService.ReadAll(cancellationToken);
+
+        return ReturnResult(r);
+    }
+}
