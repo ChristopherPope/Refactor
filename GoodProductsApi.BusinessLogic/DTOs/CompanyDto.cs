f4b6dee78194fbaa4cc4e01a4ea7e1eb199ab27f
Christopher Pope <christopher@thepopes.org>
2025-12-13 10:49:23 -0500

    Add DTO objects

---
+++ GoodProductsApi.BusinessLogic/DTOs/CompanyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GoodProductsApi.BusinessLogic.DTOs;
+
+public sealed class CompanyDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<ProductDto> Products { get; set; } = [];
+    public int ProductCount => Products.Count;
+}
---
+++ GoodProductsApi.BusinessLogic/DTOs/ProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GoodProductsApi.BusinessLogic.DTOs;
+
+public sealed class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
