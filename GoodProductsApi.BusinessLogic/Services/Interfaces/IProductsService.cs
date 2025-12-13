626d5839fcd537d74b07d8d3220bc4663fd1f519
Christopher Pope <christopher@thepopes.org>
2025-12-13 11:05:42 -0500

    Add ReadAll() to products service

---
+++ GoodProductsApi.BusinessLogic/Services/Interfaces/IProductsService.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using GoodProductsApi.BusinessLogic.DTOs;
+
+namespace GoodProductsApi.BusinessLogic.Services.Interfaces;
+
+public interface IProductsService
+{
+    Task<Result<List<CompanyDto>>> ReadAll(CancellationToken cancellationToken);
+}
--- GoodProductsApi.BusinessLogic/Services/ProductsService.cs
+++ GoodProductsApi.BusinessLogic/Services/ProductsService.cs
@@ -1,16 +1,41 @@
-﻿using GoodProductsApi.DataAccess.Persistence.Interfaces;
+﻿using FluentResults;
+using GoodProductsApi.BusinessLogic.DTOs;
+using GoodProductsApi.BusinessLogic.Mappers.Interfaces;
+using GoodProductsApi.BusinessLogic.Services.Interfaces;
+using GoodProductsApi.DataAccess.Persistence.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace GoodProductsApi.BusinessLogic.Services;
 
-internal sealed class ProductsService
+internal sealed class ProductsService : IProductsService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly ILogger<ProductsService> _logger;
+    private readonly IProductMapper _productMapper;
 
-    public ProductsService(IUnitOfWork unitOfWork, ILogger<ProductsService> logger)
+    public ProductsService(IUnitOfWork unitOfWork,
+        IProductMapper productMapper,
+        ILogger<ProductsService> logger)
     {
         _unitOfWork = unitOfWork;
+        _productMapper = productMapper;
         _logger = logger;
+    }
+
+
+    public async Task<Result<List<CompanyDto>>> ReadAll(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var productEntities = await _unitOfWork.Products.ReadAll(cancellationToken);
+
+            return _productMapper.FromEntities(productEntities);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unable to read Products");
+
+            return Result.Fail("Unable to read Products");
+        }
     }
 }
