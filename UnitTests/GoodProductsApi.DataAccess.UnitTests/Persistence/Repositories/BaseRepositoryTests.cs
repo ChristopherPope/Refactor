6549e505ebb7f691a409968177fff529843a9db1
Christopher Pope <christopher@thepopes.org>
2025-12-13 22:36:26 -0500

    Add CompanyRepository Unit Tests

---
+++ UnitTests/GoodProductsApi.DataAccess.UnitTests/Persistence/Repositories/BaseRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using GoodProductsApi.DataAccess.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace GoodProductsApi.DataAccess.UnitTests.Persistence.Repositories;
+
+internal abstract class BaseRepositoryTests
+{
+    protected DbConnection _connection = null!;
+    protected DbContextOptions<ProductsDbContext> _dbContextOptions = null!;
+    protected List<Company> _companies = [];
+    protected List<Product> _products = [];
+
+    [SetUp]
+    public void Setup()
+    {
+        _connection = new SqliteConnection("DataSource=:memory:");
+        _connection.Open();
+        _dbContextOptions = new DbContextOptionsBuilder<ProductsDbContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        using var context = new ProductsDbContext(_dbContextOptions);
+        context.Database.EnsureCreated();
+
+        _companies = new List<Company>
+        {
+            new() { Id = 1, Name = "Company A" },
+            new() { Id = 2, Name = "Company B" },
+            new() { Id = 3, Name = "Company C" }
+        };
+        context.AddRange(_companies);
+
+        foreach (var company in _companies)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                _products.Add(new Product
+                {
+                    Id = (company.Id - 1) * 3 + i,
+                    Name = $"Product {i} of {company.Name}",
+                    Price = 10.0m * i,
+                    CompanyId = company.Id
+                });
+            }
+        }
+        context.AddRange(_products);
+
+        context.SaveChanges();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _connection.Dispose();
+        _companies.Clear();
+        _products.Clear();
+    }
+
+}
---
+++ UnitTests/GoodProductsApi.DataAccess.UnitTests/Persistence/Repositories/CompanyRepositoryTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using GoodProductsApi.DataAccess.Entities;
+using GoodProductsApi.DataAccess.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Moq.AutoMock;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GoodProductsApi.DataAccess.UnitTests.Persistence.Repositories;
+
+[ExcludeFromCodeCoverage]
+internal sealed class CompanyRepositoryTests : BaseRepositoryTests
+{
+    [Test]
+    public async Task ReadById()
+    {
+        // ARRANGE
+        var autoMock = new AutoMocker();
+        using var context = new ProductsDbContext(_dbContextOptions);
+        autoMock.Use<ProductsDbContext>(context);
+        var repo = autoMock.CreateInstance<CompanyRepository>();
+
+        var expectedCompany = _companies.First();
+
+        // ACT
+        var actualCompany = await repo.ReadById(expectedCompany.Id, new CancellationToken());
+
+        // ASSERT
+        actualCompany.Should().BeEquivalentTo(expectedCompany, options => options
+            .Excluding(c => c.Products));
+    }
+
+
+    [Test]
+    public async Task ReadAll()
+    {
+        // ARRANGE
+        var autoMock = new AutoMocker();
+        using var context = new ProductsDbContext(_dbContextOptions);
+        autoMock.Use<ProductsDbContext>(context);
+        var repo = autoMock.CreateInstance<CompanyRepository>();
+
+
+        // ACT
+        var actualCompanies = await repo.ReadAll(new CancellationToken());
+
+        // ASSERT
+        actualCompanies.Should().BeEquivalentTo(_companies, options => options
+            .Excluding(c => c.Products));
+    }
+}
